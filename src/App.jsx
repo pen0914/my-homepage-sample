09750646856e6159a8f08f4e69868080615ade9f
pen0914 <115807679+pen0914@users.noreply.github.com>
2022-11-23 02:39:24 +0900

    Cartのページ遷移 history.pushは遷移するページ内では上手くいくが、不変の親コンポーネントでは失敗。 代わりに、Linkを使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { BrowserRouter, useHistory } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import "./styles.css";
 import { Router } from "./router/Router";
 
 export const App = () => {
-  const history = useHistory();
-
-  const onClickCart = () => history.push("/cart");
-
   return (
     <BrowserRouter>
       <div className="header">
@@ -15,9 +11,9 @@ export const App = () => {
         </button>
         <p className="header-name">potato</p>
 
-        <button className="btn cart" onClick={onClickCart}>
+        <Link className="btn cart" to="/cart">
           <span className="fa fa-cart-plus"></span>
-        </button>
+        </Link>
       </div>
 
       <Router />
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
 export const Cart = () => {
   return (
     <>
       <div>
+        <br />
+        <br />
         <p>カート内一覧</p>
+        <Link to="/">ホームに戻る</Link>
       </div>
     </>
   );
